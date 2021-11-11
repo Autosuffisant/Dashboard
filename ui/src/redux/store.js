bcd148f2623b2d5dcbe0a9e36745b445acc0d24b
Gabriel Huguein-Dumittan <gabriel.huguenin-dumittan@epitech.eu>
2021-11-12 00:43:00 +0100

    Lovely Redux main store with user data

---
+++ ui/src/redux/store.js
@@ -0,0 +1,38 @@
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import {
+  userReducer,
+  authenticationReducer,
+  uiReducer,
+} from './reducers/index';
+
+let middlewares;
+
+if (process.env.NODE_ENV === 'development') {
+  // middlewares = [thunk];
+  // To activate redux logs
+  middlewares = [thunk, logger];
+} else {
+  middlewares = [thunk];
+}
+
+const appReducer = combineReducers({
+  user: userReducer,
+  auth: authenticationReducer,
+  ui: uiReducer,
+});
+
+const rootReducer = (state, action) => {
+  // when a logout action is dispatched it will reset redux state
+  let newState = state;
+  if (action.type === 'USER_LOGGED_OUT') {
+    newState = {};
+  }
+
+  return appReducer(newState, action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export default store;
