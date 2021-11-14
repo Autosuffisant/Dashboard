574053d8e296f53fbdfec3725891956dcc79fd24
Gabriel Huguein-Dumittan <gabriel.huguenin-dumittan@epitech.eu>
2021-11-14 12:41:35 +0100

    Centered drag and drop cards array

--- ui/src/components/Dashboard/components/GridLayout.jsx
+++ ui/src/components/Dashboard/components/GridLayout.jsx
@@ -29,7 +29,9 @@ const GridLayout = () => {
   const HoverTitle = 'Hover !';
   const Image = '';
 
-  const list = [{ id: '0' }, { id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }, { id: '5' }];
+  const list = [
+    { id: '0' }, { id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }, { id: '5' }, { id: '6' }, { id: '7' }, { id: '8' }, { id: '9' },
+  ];
 
   useEffect(() => {
   }, []);
