2876246944a4cf6f7b6281f6565285a72bd9162f
Gabriel Huguein-Dumittan <gabriel.huguenin-dumittan@epitech.eu>
2021-11-14 10:41:55 +0100

    Widget paper element is a paper

--- ui/src/components/Dashboard/components/Widgets.jsx
+++ ui/src/components/Dashboard/components/Widgets.jsx
@@ -1,68 +1,48 @@
 import React, { useEffect } from 'react';
 import {
-  Typography,
-  Grid,
-  Tooltip,
-  Card,
-  CardActionArea,
-  CardMedia,
-  CardContent,
+  Grid, Paper,
 } from '@material-ui/core';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   appBarSpacer: theme.mixins.toolbar,
+  container: {
+    backgroundColor: '#fff',
+    height: '30vh',
+    width: '30vh',
+    margin: theme.spacing(2),
+    marginLeft: theme.spacing(8.5),
+  },
   content: {
     flexGrow: 1,
     height: '100vh',
     overflow: 'auto',
-  },
-  container: {
-    backgroundColor: theme.palette.background.paper,
-    borderRadius: 15,
-    padding: theme.spacing(4),
   },
   imgCard: {
     height: '20vh',
   },
 }));
 
-const Widgets = ({
-  Title, HoverTitle, Image,
-}) => {
+const Widgets = () => {
   const classes = useStyles();
 
   useEffect(() => {
   }, []);
 
   return (
-    <Grid item lg={2} md={4} sm={6} xs={12}>
-      <Tooltip title={HoverTitle} placement="top">
-        <Card>
-          <CardActionArea>
-            <CardMedia
-              className={classes.imgCard}
-              image={Image}
-              title="Widget"
-            />
-            <CardContent>
-              <Typography variant="body1">
-                {Title}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Tooltip>
+    <Grid item>
+      <Paper className={classes.container} />
     </Grid>
-
   );
 };
 
+Widgets.defaultProps = {
+  Title: 'Title',
+  HoverTitle: 'Widget',
+  Image: 'Image',
+};
+
 Widgets.propTypes = {
-  Title: PropTypes.string.isRequired,
-  HoverTitle: PropTypes.string.isRequired,
-  Image: PropTypes.string.isRequired,
 };
 
 export default Widgets;
