5d49a4f72a793c547db3fdfd3315938e99b3a10e
Gabriel Huguein-Dumittan <gabriel.huguenin-dumittan@epitech.eu>
2021-11-15 14:42:41 +0100

    Changed port

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,9 +1,9 @@
 const dev = {
-  API_URL: 'http://localhost:3000',
+  API_URL: 'http://localhost:8080',
 };
 
 const prod = {
-  API_URL: 'http://localhost:3000',
+  API_URL: 'http://localhost:8080',
 };
 
 const config = process.env.NODE_ENV === 'development' ? dev : prod;
