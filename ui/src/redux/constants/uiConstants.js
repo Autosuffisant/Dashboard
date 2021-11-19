e87c02a5943dcc6aba693032cad7bc46e285582e
Gabriel Huguein-Dumittan <gabriel.huguenin-dumittan@epitech.eu>
2021-11-19 23:55:34 +0100

    Color change constant

--- ui/src/redux/constants/uiConstants.js
+++ ui/src/redux/constants/uiConstants.js
@@ -1,7 +1,7 @@
 const uiConstants = {
   CHANGE_PAGE_TITLE: 'PAGE_TITLE',
   CHANGE_DARK_MODE: 'CHANGE_DARK_MODE',
-  SET_CURRENT_GROUP: 'UI_SET_CURRENT_GROUP',
+  CHANGE_PAGE_COLOR: 'CHANGE_PAGE_COLOR',
 };
 
 export default uiConstants;
