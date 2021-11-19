2d8809cd959ec44490b3f27411e000dd4ca3f6f9
Gabriel Huguein-Dumittan <gabriel.huguenin-dumittan@epitech.eu>
2021-11-19 23:55:18 +0100

    Color theme handling in UI reducer

--- ui/src/redux/reducers/uiReducer.js
+++ ui/src/redux/reducers/uiReducer.js
@@ -2,10 +2,12 @@ import uiConstants from '../constants/uiConstants';
 
 const currentPage = JSON.parse(sessionStorage.getItem('currentPage')) ? JSON.parse(sessionStorage.getItem('currentPage')) : 'Dashboard';
 const darkMode = JSON.parse(localStorage.getItem('darkMode')) ? JSON.parse(localStorage.getItem('darkMode')) : false;
+const themeColor = JSON.parse(localStorage.getItem('themeColor')) ? JSON.parse(localStorage.getItem('themeColor')) : '0011aa';
 
 const initialState = {
   currentPage,
   darkMode,
+  themeColor,
 };
 
 export default function ui(state = initialState, action) {
@@ -19,6 +21,11 @@ export default function ui(state = initialState, action) {
       return {
         ...state,
         darkMode: action.darkMode,
+      };
+    case uiConstants.CHANGE_PAGE_COLOR:
+      return {
+        ...state,
+        themeColor: action.themeColor,
       };
     default:
       return state;
