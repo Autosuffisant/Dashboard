b3bae6d642dc3a99ac5b7077d5a8b63d9dc887c2
Gabriel Huguein-Dumittan <gabriel.huguenin-dumittan@epitech.eu>
2021-11-21 23:25:06 +0100

    Widget reducer include

--- ui/src/redux/reducers/index.js
+++ ui/src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import userReducer from './userReducer';
 import authenticationReducer from './authenticationReducer';
 import uiReducer from './uiReducer';
+import widgetReducer from './widgetReducer';
 
 export {
   userReducer,
   authenticationReducer,
   uiReducer,
+  widgetReducer,
 };
