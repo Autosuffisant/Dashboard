49f1792326c14854e40021eea5ccd924e85306de
Gabriel Huguein-Dumittan <gabriel.huguenin-dumittan@epitech.eu>
2021-11-21 23:21:39 +0100

    Services for getting and setting widget in DB

---
+++ ui/src/redux/services/widgetServices.js
@@ -0,0 +1,25 @@
+import {
+  apiClient,
+  handleResponse,
+} from './axios';
+
+function getWidgets(user) {
+  return apiClient.get(`/user/widgets/${user}`)
+    .then(handleResponse)
+    .then((res) => res)
+    .catch((error) => Promise.reject(error.response.data.message));
+}
+
+function updateWidgets(user, widgets) {
+  return apiClient.put(`/user/widgets/${user}`, widgets)
+    .then(handleResponse)
+    .then((res) => res)
+    .catch((error) => Promise.reject(error.response.data.message));
+}
+
+const widgetServices = {
+  getWidgets,
+  updateWidgets,
+};
+
+export default widgetServices;
